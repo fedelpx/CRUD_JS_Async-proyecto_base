46163460f98b57d2406e8977703dc42105333d05
Federico Carranza Zavalia <fedecarranzanz@gmail.com>
2023-07-27 14:50:58 -0300

    Borrar cliente

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -8,7 +8,7 @@ formulario.addEventListener("submit", (evento) => {
     const email = document.querySelector("[data-email]").value
     clientServices
     .crearCliente(nombre, email)
-    .then ( (respuesta) =>{
+    .then (() =>{
     window.location.href="../screens/registro_completado.html"
     })
     .catch((err) => console.log(err))
